9af4f94aaddc851180fff90e6a2cf83cfb394e80
luomazhang <88522712>
2015-02-22 20:38:02 -0500

    proto

--- BingSearch.java
+++ BingSearch.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
-
+import java.util.*;
 
 //Download and add this library to the build path.
 import org.apache.commons.codec.binary.Base64;
@@ -10,9 +10,10 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class BingSearch {
-
-	public static void main(String[] args) throws IOException {
-		String bingUrl = "https://api.datamarket.azure.com/Bing/Search/Web?Query=%27Milky%20Way%27&$top=10&$format=JSon";
+	public static ArrayList<BingObject> search(String keyWords,float precision) throws IOException {
+		keyWords = keyWords.trim();
+		String words = keyWords.replaceAll(" ","%20");
+		String bingUrl = "https://api.datamarket.azure.com/Bing/Search/Web?Query=%27"+words+"%27&$top=10&$format=JSon";
 		//Provide your account key here. 
 		String accountKey = "le5VXjKjrgc7nRivO9KXOSU+vKyaMr4cnFnktYD8dQE";
 		byte[] accountKeyBytes = Base64.encodeBase64((accountKey + ":" + accountKey).getBytes());
@@ -28,20 +29,27 @@ public class BingSearch {
 		String content = new String(contentRaw);
 		JSONObject json = new JSONObject(content);
 		JSONArray result = json.getJSONObject("d").getJSONArray("results");
+		ArrayList<BingObject> bol = new ArrayList<BingObject>();
 		for(int i=0;i<result.length();i++){
 			JSONObject curr = result.getJSONObject(i);
-			String uri = curr.getJSONObject("__metadata").getString("uri");
-			String id = curr.getString("ID");
 			String title = curr.getString("Title");
 			String description = curr.getString("Description");
-			String displayUrl = curr.getString("DisplayUrl");
 			String myUrl = curr.getString("Url");
-			System.out.println(title);
-			System.out.println(description);
-			System.out.println();
+			bol.add(new BingObject(title,description,myUrl));
 		}
+		return bol;
 		//The content string is the xml/json output from Bing.
 		//System.out.println(content);
 	}
 
+}
+class BingObject{
+	public String title;
+	public String description;
+	public String url;
+	public BingObject(String title,String description,String url){
+		this.title=title;
+		this.description=description;
+		this.url=url;
+	}
 }
