9d93694ed3ea816575353563b4eb0b18e8966f6b
曹 怀元 <caohuaiyuan@soutekiMacBook-Pro.local>
2015-02-23 21:36:52 -0500

    shitttt

---
+++ README.txt
@@ -0,0 +1,23 @@
+1.Teammate:
+Name: Huaiyuan Cao UNI:hc2748
+Name: Luoma Zhang  UNI:lz2413
+
+2.Files submitted:
+BingSearch.java
+IRHelper.java
+test.java
+stopwords.txt
+README.txt
+commons-codec-1.10.jar
+json-20140107.jar
+
+3.How to run:
+First Compile: javac -cp "./commons-codec-1.10.jar:./json-20140107.jar" *.java
+Then Run test: java -cp .:commons-codec-1.10.jar:json-20140107.jar test
+
+Then based on the guide, input precision and query, such as:
+your precision: 0.9
+your key words: Milky Way 
+
+
+6.Our Bing Search Account Key: le5VXjKjrgc7nRivO9KXOSU+vKyaMr4cnFnktYD8dQE
--- test.java
+++ test.java
@@ -17,9 +17,12 @@ public class test {
 			}
 			int count=0;
 			for(int i = 0; i< result.size(); i++){
-				System.out.println("--------------- No. "+i+" -------------------");
+				int index = i+1;
+				System.out.println();
+				System.out.println("--------------- No. "+index+" -------------------");
 				System.out.println(result.get(i).title);
 				System.out.println(result.get(i).description);
+				System.out.println(result.get(i).url);
 				System.out.println("do you think it is relevant? Y/N");
 				if(stdin.nextLine().toLowerCase().equals("y")){
 					result.get(i).setRelevant();
@@ -28,23 +31,27 @@ public class test {
 			}
 			if(count==0){
 				System.out.println("precision is 0, I can only help you to here =.=");
+				System.out.println();
 				System.exit(1);
 			}else if(count<precision*result.size()){
 				try {
 					String[] tmp = IRHelper.generateNewKey(result, query);
-					double[] score = IRHelper.getScore(result, query);
-					for(int i = 0; i< score.length; i++){
-						System.out.println(score[i]);
-					}
+					System.out.println("=====================");
+					System.out.println("FEEDBACK");
+					System.out.println("precision:"+(float)count/10);
 					System.out.println("we are going to add new key words: "+tmp[0]+" "+tmp[1]);
+					System.out.println();
 					query+=" "+tmp[0]+" "+tmp[1];
+					count = 0;
 					//System.out.println(tmp[0]+"\n"+tmp[1]);
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}else{
+				System.out.println("precision:"+(float)count/10);
 				System.out.println("precision is high enough, byebye =.=");
+				System.out.println();
 				System.exit(1);
 			}
 		}
